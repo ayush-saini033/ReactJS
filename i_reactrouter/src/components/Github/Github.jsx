df9d3eddcb98ac4682a6bf62aa654b40268e46c9
Ayush <ayushsaini1910@gmail.com>
2024-08-18 12:56:31 +0530

    web

---
+++ i_reactrouter/src/components/Github/Github.jsx
@@ -0,0 +1,31 @@
+import React, {useEffect} from 'react'
+import { useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
+function Github() {
+    const data = useLoaderData()
+
+    // const [data, setData] = useState([])
+
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/hiteshchoudhary')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         setData(data)
+    //     })
+    // }, [])
+
+  return (
+    <div className='text-center m-4 bg-gray-600 text-white p-4 text-3xl'>
+      Github followers: {data.followers}
+      <img className='text-center' src={data.avatar_url} alt="Git picture"  width={300} />
+    </div>
+  )
+}
+
+export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/hiteshchoudhary')
+    return response.json()
+}
